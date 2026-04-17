e2ca079e0cdfd1af729a7bec71de7df03f0f2f90
Tory L <lysiktory@gmail.com>
2026-04-17 17:04:32 -0400

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -22,16 +22,19 @@
 	<div class="container">
 		<div class="bio">
 			<p>
-				I'm a data and graphics journalist and web developer based in New York City. Currently I am a Reporter at the Tow Center for Digital Journalism. I'm passionate about using data and graphics to uncover the stories that impact lives and build a better world. I love talking to real human beings, making order from chaos, and figuring out seemingly unsolvable problems. I also care deeply about accessibility — taking insanely complex topics and critical information and making it clear and understandable.
+				I'm a data and graphics journalist and web developer based in New York City.
 			</p>
 			<p>
-				My career has sometimes followed a bit of a zigzag path, but I've worked at outlets including <a href="https://www.axios.com/results?q=tory%20lysik&sort=2" target="_blank" rel="noopener noreferrer">Axios</a>, <a href="https://www.vox.com/" target="_blank" rel="noopener noreferrer">Vox</a>, <a href="https://www.themarshallproject.org/" target="_blank" rel="noopener noreferrer">The Marshall Project</a>, the <a href="https://apnews.com/" target="_blank" rel="noopener noreferrer">Associated Press</a>, and others. My work spans from local and national shoeleather reporting to interactive graphics and tools. The work I've been a part of has received numerous awards, and has been cited in congressional testimony and federal legislation.
+				Currently, I am a reporter at the Tow Center for Digital Journalism. I am passionate about using data and graphics to uncover the stories that impact lives and build a better world. I love speaking with real human beings, making order from chaos, and figuring out seemingly unsolvable problems. I also care deeply about accessibility — taking insanely complex topics and critical information and making it clear and understandable.
+			</p>
+			<p>
+				My career has sometimes followed a bit of a zigzag path. I've worked at outlets including <a href="https://www.axios.com/results?q=tory%20lysik&sort=2" target="_blank" rel="noopener noreferrer">Axios</a>, <a href="https://www.vox.com/" target="_blank" rel="noopener noreferrer">Vox</a>, <a href="https://www.themarshallproject.org/" target="_blank" rel="noopener noreferrer">The Marshall Project</a>, the <a href="https://apnews.com/" target="_blank" rel="noopener noreferrer">Associated Press</a>, and others. My work spans from local and national shoeleather reporting to interactive graphics and tools. The work I've been a part of has received numerous awards, and has been cited in congressional testimony and federal legislation.
 			</p>
 			<p>
 				I love what I do. Whether it's diving down rabbit holes in legal documents, filing the perfect FOIA request, or building detailed maps to surface human rights issues, I thrive in the meticulous, detail-heavy work that makes complex stories click — on tight deadlines or over months.
 			</p>
 			<p>
-				In my free time, I'm usually escaping the concrete jungle of NYC for the mountains or trying to become fluent in a fifth language.
+				In my free time, I am usually escaping the concrete jungle of NYC for the mountains or trying to become fluent in a fifth language.
 			</p>
 		</div>
 	</div>
